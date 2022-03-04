7ed115c139f5d19e17fa741fa9d850ee3d30e547
Margaux <margaux.malsch@edu.devinci.fr>
2022-03-04 10:50:19 +0100

    :sparkles: added bootstrap

--- vivepython/settings.py
+++ vivepython/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'caprisun.apps.CaprisunConfig',
     'shop.apps.ShopConfig',
     'channels',
+    'bootstrap5',
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
